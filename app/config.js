2dfb34d8932824dd0fcb83e72c1a596aaacc0fbd
Jan-Kees van Andel <j.c.c.andel@rn.rabobank.nl>
2014-09-30 11:21:38 -0700

    Initial commit

---
+++ app/config.js
@@ -0,0 +1,6 @@
+/*
+  baseUri is is produced dynamically when deployed on cloudbees node.js server based on the 'API' configuration parameter
+*/
+
+var baseUri = 'http://localhost/v2/';
+var baseUriV1 = 'http://localhost/rest/v1/';
---
+++ app/scripts/config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('Config', []).constant('ConfigAPI', {
+
+});
