13992c978f0e76580cdfe5877c2d9ed026b8f409
Jan-Kees van Andel <j.c.c.andel@rn.rabobank.nl>
2014-10-30 21:10:31 +0100

    Switched back to online REST services

--- app/config.js
+++ app/config.js
@@ -2,5 +2,5 @@
   baseUri is is produced dynamically when deployed on cloudbees node.js server based on the 'API' configuration parameter
 */
 
-var baseUri = 'http://localhost/v2/';
-var baseUriV1 = 'http://localhost/rest/v1/';
+var baseUri = 'http://cfp.devoxx.com/v2/';
+var baseUriV1 = 'http://cfp.devoxx.com/rest/v1/';
