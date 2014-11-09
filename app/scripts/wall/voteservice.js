e5247882703c14fb50ed03741019159cf4caac78
Jan-Kees van Andel <j.c.c.andel@rn.rabobank.nl>
2014-11-09 13:40:37 +0100

    Removed dummy voting testdata

--- app/scripts/wall/voteservice.js
+++ app/scripts/wall/voteservice.js
@@ -7,43 +7,23 @@ angular.module('VotingService', []).factory('VotingService', ['$http', '$q', '$t
         topOfWeek: function() {
             var defer = $q.defer();
 
-            $timeout(function() {
-                var data = [
-                    { talkId: "1420" },
-                    { talkId: "1704" },
-                    { talkId: "1705" },
-                    { talkId: "1712" }
-
-                ];
+            $http.get(topOfWeekUrl).success(function(data, status, headers, config) {
                 defer.resolve(data);
-            }, 5);
+            }).error(function(data, status, headers, config) {
+                defer.reject(data);
+            });
 
-            //$http.get(topOfWeekUrl).success(function(data, status, headers, config) {
-            //    defer.resolve(data);
-            //}).error(function(data, status, headers, config) {
-            //    defer.reject(data);
-            //});
             return defer.promise;
         },
         topOfDay : function(callback) {
             var defer = $q.defer();
 
-            $timeout(function() {
-                var data = [
-                    { talkId: "1712" },
-                    { talkId: "1805" },
-                    { talkId: "1810" },
-                    { talkId: "1832" }
-
-                ];
+            $http.get(topOfDayUrl).success(function(data, status, headers, config) {
                 defer.resolve(data);
-            }, 5);
+            }).error(function(data, status, headers, config) {
+                defer.reject(data);
+            });
 
-            //$http.get(topOfDayUrl).success(function(data, status, headers, config) {
-            //    defer.resolve(data);
-            //}).error(function(data, status, headers, config) {
-            //    defer.reject(data);
-            //});
             return defer.promise;
         }
     }
